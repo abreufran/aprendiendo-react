a9a9cf1ae5e089428c7d76a60c23bf8cd0ae2e77
Francisco Abreu <francisco.abreu@externos-cl.cencosud.com>
2021-05-25 10:10:49 -0400

    Hola Mundo desde React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+function holaMundo(nombre, edad) {
+  var presentacion2 = (
+    <p>
+      Hola Mundo desde una Función. Soy, {nombre}<br></br>
+      Tengo {edad} años.
+    </p>
+  );
+  return presentacion2;
+}
+
 function App() {
+  var nombre = "Francisco Abreu";
+  var presentacion = <h2>Hola, soy {nombre}</h2>
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hola Mundo con React!!! <br></br><br></br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {presentacion}
+        {holaMundo(nombre, 40)}
         <a
           className="App-link"
           href="https://reactjs.org"
